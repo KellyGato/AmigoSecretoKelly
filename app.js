fc76784214b1d11a8451caabe95044279e643661
KellyGato <kellycristina_dng@hotmail.com>
2025-09-21 22:15:27 -0300

    Commi da conclusão da aula

---
+++ app.js
@@ -0,0 +1,52 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let nomeInput = document.getElementById('amigo');
+    let nome = nomeInput.value.trim();
+    console.log(nomeInput.value);
+
+    if (!nome) {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+    
+    amigos.push(nome);
+    atualizarLista();
+    nomeInput.value = '';
+    nomeInput.focus();
+}
+
+function atualizarLista() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Não há amigos para ser sorteado. Adicione nomes para o sorteio.');
+        return;
+    }
+    
+    let sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `O amigo secreto sorteado foi: <li>${sorteado}</li>`;
+}
+
+// Adicionar nome no sorteio com a tecla "enter"
+let nomeInput = document.getElementById('amigo');
+nomeInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') adicionarAmigo();
+});
+
+
+
+
+
+
+
